7ed97dc9d96b9f0b999b9b0a5f5ff4c6a110912d
Moliner Eloi <e.moliner.em@gmail.com>
2024-06-05 15:06:07 +0000

    commenting the import of the fancy kernels from NCSNpp. That would save people from some headaches, as it eliminates the need of compiling the kernels

--- networks/ncsnpp_utils/up_or_down_sampling.py
+++ networks/ncsnpp_utils/up_or_down_sampling.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 import numpy as np
-from .op import upfirdn2d, upfirdn1d
+#from .op import upfirdn2d, upfirdn1d
 
 
 # Function ported from StyleGAN2
